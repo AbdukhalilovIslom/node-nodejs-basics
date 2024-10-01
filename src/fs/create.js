172fddad8d0e7c7f721f124545ecfb3b6e88d422
AbdukhalilovIslom <abdukhalilovislom2021@gmail.com>
2024-10-01 21:56:12 +0500

    create on process

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,6 +1,13 @@
+import fs from "fs";
+
 const create = async () => {
-    // Write your code here 
-    // test coomment
+  fs.readFile("./files/fresh.txt", "utf8", (error, data) => {
+    if (data) {
+      throw new Error("FS operation failed");
+    } else {
+      fs.writeFile("./files/fresh.txt", "I am fresh and young", "utf-8");
+    }
+  });
 };
 
 await create();
