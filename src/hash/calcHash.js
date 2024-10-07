a7245def04ccc51f8e6615cc02d361637b959d7e
AbdukhalilovIslom <abdukhalilovislom2021@gmail.com>
2024-10-08 00:49:27 +0500

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import { createHash } from "crypto";
+import { createReadStream } from "fs";
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const pathToFile = path.join(
+    __dirname,
+    "files",
+    "fileToCalculateHashFor.txt"
+  );
+  const hash = createHash("sha256");
+  const stream = createReadStream(pathToFile);
+  stream.on("data", (data) => {
+    hash.update(data);
+  });
+  stream.on("end", () => {
+    const result = hash.digest("hex");
+    console.log(`SHA256 Hash: ${result}`);
+  });
+  stream.on("error", (err) => {
+    throw new Error("FS operation failed", err);
+  });
 };
 
 await calculateHash();
