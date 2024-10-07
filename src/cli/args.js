79a32ff8c1342cd59239aa598f76b2d954bb87ba
AbdukhalilovIslom <abdukhalilovislom2021@gmail.com>
2024-10-08 00:52:52 +0500

    feat: cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  args.forEach((item, i, array) => {
+    if (i == 0 || i % 2 == 0) {
+      console.log(`${item} is ${array[i + 1]}`);
+    }
+  });
 };
 
 parseArgs();
